28b021558044a6db391f5ef6cc81369094b3cfa9
Morisson Marcel <11509104+morissonmaciel@users.noreply.github.com>
2024-11-04 11:04:03 -0300

    -- Post script helper

--- rootfs/etc/profile.d/user.sh
+++ rootfs/etc/profile.d/user.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-usermod -aG wheel $(whoami)
-sed -i "s/^User=.*/User=$(whoami)/" /etc/sddm.conf.d/steamos.conf
+_post_install_helper() {
+    sudo usermod -aG wheel $(whoami)
+    sudo sed -i "s/^User=.*/User=$(whoami)/" /etc/sddm.conf.d/steamos.conf
+    sudo systemctl disable gdm
+    sudo systemctl enable sddm
+
+    echo "Please reboot your system to apply changes."
+}
+
+alias userps=_post_install_helper
